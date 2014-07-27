17560a9dc8f29cb32b8ee91a83d19204845cec33
Maisie Wang <maimaisie@gmail.com>
2014-07-27 16:04:46 -0400

    Remove unused files. Modify main to plot graph

--- Tabu/Main.m
+++ Tabu/Main.m
@@ -1,4 +1,15 @@
-[bestSoln, solnCost] = TabuSearch(1, 2,2, 2);
+x=[];
+y=[];
+count=1;
+for numTurbine=5:20
+    %run each case 5 times and take the best one
+    sum=0;
+    for i=1:5
+        [bestSoln, solnCost] = TabuSearch(5, 100 ,100,numTurbine );
+        sum=solnCost+sum;
+    end
+    x(count) = numTurbine;
+    y(count) = sum/5; %take average
+end
 
-solnCost
-bestSoln
+plot(x, y);
