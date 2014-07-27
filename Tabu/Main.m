0e7da056dd93182b7002cf6d94b3608a88dddfbd
Maisie Wang <maimaisie@gmail.com>
2014-07-27 16:08:14 -0400

    Modify main

--- Tabu/Main.m
+++ Tabu/Main.m
@@ -2,7 +2,7 @@ x=[];
 y=[];
 count=1;
 for numTurbine=5:20
-    %run each case 5 times and take the best one
+    %run each case 5 times and take average
     sum=0;
     for i=1:5
         [bestSoln, solnCost] = TabuSearch(5, 100 ,100,numTurbine );
@@ -10,6 +10,7 @@ for numTurbine=5:20
     end
     x(count) = numTurbine;
     y(count) = sum/5; %take average
+    count=count+1;
 end
 
 plot(x, y);
